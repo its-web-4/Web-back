705d4a3ea099330157dd3e726cc1fe595176a544
eastminnn <oh1334000@gmail.com>
2025-02-15 16:35:51 +0900

    feat : 프로젝트 초기 환경 세팅

---
+++ src/main/java/com/web_4/StockNote/StockNoteApplication.java
@@ -0,0 +1,13 @@
+package com.web_4.StockNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web_4/StockNote/StockNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_4.StockNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
