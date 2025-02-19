56c74f1337ce61085a5df1fda799e586ec403c76
eastminnn <oh1334000@gmail.com>
2025-02-19 22:57:52 +0900

    feat : 권한 기본값 추가

--- src/main/java/com/web_4/StockNote/user/dto/UserRegisterRequestDto.java
+++ src/main/java/com/web_4/StockNote/user/dto/UserRegisterRequestDto.java
@@ -3,6 +3,8 @@ package com.web_4.StockNote.user.dto;
 import com.web_4.StockNote.user.entity.User;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class UserRegisterRequestDto {
     private String userEmail;
@@ -15,6 +17,8 @@ public class UserRegisterRequestDto {
                 .userEmail(this.userEmail)
                 .userName(this.userName)
                 .userPassword(this.userPassword)  // 비밀번호는 암호화 필수
+                .role(User.Role.USER)
+                .createdAt(LocalDateTime.now())
                 .build();
     }
 }
