4419ad003153874d9fd00ee63506d2fe6cf468d8
eastminnn <oh1334000@gmail.com>
2025-02-22 19:13:17 +0900

    feat : stock entity

---
+++ src/main/java/com/web_4/StockNote/stock/entity/Stock.java
@@ -0,0 +1,51 @@
+package com.web_4.StockNote.stock.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long stockId; // 주식 고유 id
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String ticker; // 주식 코드
+
+    @Column(nullable = false, length = 100)
+    private String companyName; // 회사이름
+
+    @Column(nullable = false, length = 50)
+    private String marketType; // 코스피, 코스닥 등
+
+    @Column(length = 50)
+    private String sector; // 업종
+
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal currentPrice; // 현재 가격
+
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal highestPrice; // 최고가
+
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal lowestPrice; // 최저가
+
+    @Column(nullable = false, precision = 7, scale = 3)
+    private BigDecimal changeRate; // 변동성
+
+    @Column(nullable = false)
+    private Long volume; // 거래량
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt; // 업데이트 시간
+
+    @Column(nullable = false)
+    private String source; // API 출처
+
+    // @OneToMany(mappedBy = "stock", cascade = CascadeType.ALL, orphanRemoval = true)
+    //  private List<Watchlist> watchlist = new ArrayList<>();
+}
